69684c304fb7d000cd0f84a906132fc437131171
bappi2097 <bappi35-2097@diu.edu.bd>
2020-12-18 22:23:46 +0600

    js continue

---
+++ js-practice/5. javascript-practice/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("root").innerHTML = "Bappi Saha";
+// window.alert("Hello world");
+document.write("Hello world");
+console.log("Hello world");
---
+++ js-practice/Task-1-Celsius/script.js
@@ -0,0 +1,4 @@
+    var c = window.prompt("Temperature in Celcius");
+    var f = 9/512*c+32;
+    alert("Fahrenheit: "+f+" Degree");
+    console.log("Task Complete");
