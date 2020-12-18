4700ae630bf2795b7810211f5c811413bb62be2c
bappi2097 <bappi35-2097@diu.edu.bd>
2020-12-18 23:51:38 +0600

    object method complete

---
+++ js-practice/8. javascript-functions/object-methods.js
@@ -0,0 +1,12 @@
+document.querySelector('title').innerHTML="Object Methods";
+let person = {
+    firstname: "John",
+    lastname: "Doe",
+    dob: "20-11-1998",
+    fullname: function(){
+        return `${this.firstname} ${this.lastname}`;
+    },
+}
+
+console.log(person.firstname);
+console.log(person.fullname())
